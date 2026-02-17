ecfcaf1a7e6fd5b4ddd409f11ff91e24c0932c26
Samet Yeşilot <sametyesilotiletisim@gmail.com>
2026-02-17 11:17:15 +0300

    chore : initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div >
+4 <h1 >Web Tasarimi ve Programlama </h1 >
+5 <h2 >LAB -1 </h2 >
+6 <p>Ad Soyad : [ samet ]</p>
+7 <p> Ogrenci No: [ 230541036 ]</p>
+8 </div >
+  )
+}
+
+export default App
