93b609f274e8ec015981e304f406378e1a7d479a
Samet Yeşilot <sametyesilotiletisim@gmail.com>
2026-02-17 11:23:19 +0300

    soyad ekledim

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ function App() {
 
   return (
     <div >
-4 <h1 >Web Tasarimi ve Programlama </h1 >
-5 <h2 >LAB -1 </h2 >
-6 <p>Ad Soyad : [ samet ]</p>
-7 <p> Ogrenci No: [ 230541036 ]</p>
-8 </div >
+<h1 >Web Tasarimi ve Programlama </h1 >
+<h2 >LAB -1 </h2 >
+<p>Ad Soyad : [ samet yeşilot ]</p>
+<p> Ogrenci No: [ 230541036 ]</p>
+ </div >
   )
 }
 
